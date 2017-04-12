008c1d11798ca6cb391a608249ab3b9413a4e498
· Aquila · <apocalaeon@hotmail.com>
2017-04-12 15:54:48 +1200

    Part 1 done!

--- src/WADAssignment1/Models/Order.cs
+++ src/WADAssignment1/Models/Order.cs
@@ -4,6 +4,7 @@ namespace WADAssignment1.Models
 {
 	public class Order
 	{
+		public int ID { get; set; }
 		public ICollection<Bag> Bags { get; set; }
 		public int CustomerID { get; set; }
 		public double Subtotal { get; set; }
